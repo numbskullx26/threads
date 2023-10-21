05811d82f551cf970fa0afd759012479c50cf898
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2023-10-21 21:55:23 +0530

    Firebase Auth, SignIn,SignUp added.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
+import addData from '@/firebase/firestore/addData'
 import Image from 'next/image'
 
 export default function Home() {
+
+  const handleForm = async () => {
+    const data = {
+      name: 'John snow',
+      house: 'Stark'
+    }
+    const { result, error } = await addData('users', 'user-id', data)
+
+    if (error) {
+      return console.log(error)
+    }
+  }
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
